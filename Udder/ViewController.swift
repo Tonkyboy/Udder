9969d4aa1f382bf68911e629f86dbc9d46752d63
Alex Reute <reute@gmx.net>
2020-07-17 14:20:58 +0200

    vzhbunimo

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Alex Reute on 17.07.20.
+//  Copyright © 2020 Alex Reute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
